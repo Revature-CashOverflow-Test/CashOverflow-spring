149ca13e478f4fb10a264ff4986b7cbdbf40d8be
nrblichf <nrblichf@mtu.edu>
2022-04-19 00:38:44 -0500

    added missing test coverage to SettingsServices

--- src/test/java/com/revature/service/SettingsServiceTest.java
+++ src/test/java/com/revature/service/SettingsServiceTest.java
@@ -1,5 +1,6 @@
 package com.revature.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -27,7 +28,7 @@ class SettingsServiceTest {
 	}
 	
 	@Test
-	void testChangePassword() {
+	void testChangePasswordSuccess() {
 		String username = "test";
 		String password = "test";
 		
@@ -35,9 +36,25 @@ class SettingsServiceTest {
 		
 		assertEquals(1, serv.changePassword(username, password));
 	}
+	
+	@Test
+	void testChangePasswordNoUsername() {
+		String username = null;
+		String password = "tongue";
+		
+		assertEquals(0, serv.changePassword(username, password));
+	}
+	
+	@Test
+	void testChangePasswordNoPassword() {
+		String username = "Glue";
+		String password = null;
+		
+		assertEquals(0, serv.changePassword(username, password));
+	}
 
 	@Test
-	void testChangeEmailSettings() {
+	void testChangeEmailSettingsSuccess() {
 		String username = "test";
 		boolean emailToggle = true;
 		double emailValue = 50.0;
@@ -45,5 +62,16 @@ class SettingsServiceTest {
 		when(repo.changeEmailSettings(username, emailToggle, emailValue)).thenReturn(1);
 		
 		assertTrue(serv.changeEmailSettings(username, emailToggle, emailValue));
+	}
+	
+	@Test
+	void testChangeEmailSettingsFailure() {
+		String username = "Glue";
+		boolean emailToggle = false;
+		double emailValue = 420.00;
+		
+		when(repo.changeEmailSettings(username, emailToggle, emailValue)).thenReturn(0);
+		
+		assertFalse(serv.changeEmailSettings(username, emailToggle, emailValue));
 	}
 }
