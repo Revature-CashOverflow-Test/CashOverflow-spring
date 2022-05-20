e5e9762ad1d16550e6f4f0b4ed4a97cb0fb879d8
Avery Grant <aniylaazaiahgrant@gmail.com>
2022-05-20 15:30:22 -0400

    Added create savings account feature and implementation steps

--- src/test/java/com/revature/tests/TrackMultipleAccts.java
+++ src/test/java/com/revature/tests/TrackMultipleAccts.java
@@ -19,7 +19,7 @@ public class TrackMultipleAccts {
 	   this.setUp.pageController.createBankAccountPage.sendInputToNameForm(null);
 	}
 
-	@When("the User inputs the date into a corresponding form")
+	@When("the User inputs the data into a corresponding form")
 	public void the_user_inputs_the_date_into_a_corresponding_form() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
@@ -42,4 +42,19 @@ public class TrackMultipleAccts {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	
+
+	@When("the User selects savings account")
+	public void the_user_selects_savings_account() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the application shows a new savings account is created")
+	public void the_application_shows_a_new_savings_account_is_created() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
 }
--- src/test/resources/Cucumber/TrackMultipleAccts.feature
+++ src/test/resources/Cucumber/TrackMultipleAccts.feature
@@ -6,8 +6,19 @@ Feature: Track Multiple Accounts
 	Scenario: Create a Checking Account
 	Given the User logs in successfully
 	When the User clicks on create a bank account
-	And the User inputs the date into a corresponding form
+	And the User inputs the data into a corresponding form
 	And the User selects checking account 
 	And the User clicks create account
 	Then the application shows a new checking account is created
+	
+	@TrackMultiAccounts2
+	Scenario: Create a Savings Account
+	Given the User logs in successfully
+	When the User clicks on create a bank account
+	And the User inputs the data into a corresponding form
+	And the User selects savings account 
+	And the User clicks create account
+	Then the application shows a new savings account is created
+	
+	
 	
