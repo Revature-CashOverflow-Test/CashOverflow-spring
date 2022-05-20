f7af2b5bc0694bdd64475f0f216fccf64c9a4ce5
timmy.wang777 <timmy.wang777@gmail.com>
2022-05-20 11:27:41 -0700

    add javascriptexecutor

--- src/test/java/com/revature/stepdef/SetUp.java
+++ src/test/java/com/revature/stepdef/SetUp.java
@@ -1,7 +1,8 @@
-package com.revature.tests;
+package com.revature.stepdef;
 
 import java.io.File;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,11 +11,13 @@ import com.revature.pages.PageController;
 
 public class SetUp {
 	WebDriver driver;
+	JavascriptExecutor js;	
 	public PageController pageController;
 	public SetUp() {
 		File file = new File("src/test/resources/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());//Environmental variable
 		driver = new ChromeDriver(); //default to find environmental variable 
+		js = (JavascriptExecutor)driver;
 		pageController = new PageController(driver);
 	}
 }
--- src/test/resources/Cucumber/Example.feature
+++ src/test/resources/Cucumber/Example.feature
@@ -10,3 +10,11 @@ Feature: Example
     And happy face
 
 
+	Scenario: Login Successfully
+		Given the User is in homepage
+		When the User clicks on "Login"
+		And The User inputs Username
+		And The User Inputs Password
+		And The User clicks on LogIn button
+		Then the application will have a green pop up
+	
