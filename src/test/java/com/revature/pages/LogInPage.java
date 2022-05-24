713e5d5972ba1eae78b5d82e794db3b276db7e47
timmy.wang777 <timmy.wang777@gmail.com>
2022-05-24 14:15:25 -0700

    check logIn should be fine now

--- src/test/java/com/revature/pages/LogInPage.java
+++ src/test/java/com/revature/pages/LogInPage.java
@@ -17,52 +17,50 @@ public class LogInPage {
 	public LogInPage(WebDriver driver) {
 		this.driver = driver;
 	}
-public void inputIntoUsername(String str){
-        driver.findElement(By.xpath("//*[@id=\"exampleInputEmail1\"]")).sendKeys(str); }
-
-public void inputIntoPassword(String str){
-        driver.findElement(By.xpath("//*[@id=\"exampleInputPassword1\"]")).sendKeys(str);}
-
-public void clickLogInButton(){
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        WebElement ele = wait.until(ExpectedConditions.elementToBeClickable(
-                        By.xpath("/html/body/app-root/app-login-page/app-login/div/div/div/div/div/div/form/div/input")));
-        ele.sendKeys(Keys.ENTER);
-}
-public boolean invalidCredentialsMessage(){
-        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-                        .withTimeout(Duration.ofSeconds(5))
-                        .pollingEvery(Duration.ofMillis(250))
-                        .ignoring(NoSuchElementException.class);
-        WebElement ele = wait.until(ExpectedConditions.presenceOfElementLocated(
-                        By.xpath("/html/body/div/div/div")));
-        String text = ele.getText().toString();
-        System.out.println(text);
-        return text.contains("Password/Username authentication erro"); 
-        
-}
-
-public boolean logInSuccessfullyMessage() {
-	Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-			  .withTimeout(Duration.ofSeconds(2))
-			  .pollingEvery(Duration.ofMillis(250))
-			  .ignoring(NoSuchElementException.class);
-	WebElement ele = wait.until(ExpectedConditions.presenceOfElementLocated(
-			By.xpath("/html/body/div/div/div")));
-	String text = ele.getText().toString();
-	System.out.println(text);
-	return text.contains("You have been successfully logged in");
-
-//return this.driver.getCurrentUrl().equals("http://localhost:4200/feed");
-}
-
-public void clickLogOutButton(){
-    WebDriverWait wait = new WebDriverWait(driver, 5);
-    WebElement ele = wait.until(ExpectedConditions.presenceOfElementLocated(
-                    By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[7]/a"
-                    		+ "")));
-    ele.click();
-}
+	public void inputIntoUsername(String str){
+	        driver.findElement(By.xpath("//*[@id=\"exampleInputEmail1\"]")).sendKeys(str); }
+	
+	public void inputIntoPassword(String str){
+	        driver.findElement(By.xpath("//*[@id=\"exampleInputPassword1\"]")).sendKeys(str);}
+	
+	public void clickLogInButton(){
+	        WebDriverWait wait = new WebDriverWait(driver, 5);
+	        WebElement ele = wait.until(ExpectedConditions.elementToBeClickable(
+	                        By.xpath("/html/body/app-root/app-login-page/app-login/div/div/div/div/div/div/form/div/input")));
+	        ele.sendKeys(Keys.ENTER);
+	}
+	public boolean invalidCredentialsMessage(){
+	        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+	                        .withTimeout(Duration.ofSeconds(3))
+	                        .pollingEvery(Duration.ofMillis(100))
+	                        .ignoring(NoSuchElementException.class);
+	        WebElement ele = wait.until(ExpectedConditions.presenceOfElementLocated(
+	                        By.xpath("//*[@id=\\\"toast-container\\\"]/div/")));
+	        String text = ele.getText().toString();
+	        System.out.println(text);
+	        return text.contains("Password/Username authentication erro"); 
+	        
+	}
+	
+	public boolean logInSuccessfullyMessage() {
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+				  .withTimeout(Duration.ofSeconds(3))
+				  .pollingEvery(Duration.ofMillis(100))
+				  .ignoring(NoSuchElementException.class);
+		WebElement ele = wait.until(ExpectedConditions.presenceOfElementLocated(
+				By.xpath("//*[@id=\"toast-container\"]/div/div[2]")));
+		String text = ele.getText().toString();
+		System.out.println(text);
+		return text.contains("You have been successfully logged in");
+	}
+	
+	public void clickLogOutButton(){
+	    WebDriverWait wait = new WebDriverWait(driver, 5);
+	    WebElement ele = wait.until(ExpectedConditions.presenceOfElementLocated(
+	                    By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[7]/a"
+	                    		+ "")));
+	    ele.click();
+	}
 
 
 }
--- src/test/java/com/revature/stepdef/CommonStepDef.java
+++ src/test/java/com/revature/stepdef/CommonStepDef.java
@@ -1,7 +1,11 @@
 package com.revature.stepdef;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
 
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
@@ -25,10 +29,11 @@ public class CommonStepDef {
 	@Given("the User is in homepage")
 	public void the_user_is_in_homepage() {
 		setUp.driver.get("http://localhost:4200/");
+		setUp.js.executeScript("return sessionStorage.removeItem(\"username\");");
 	}
 	@Given("the User is not logged in")
 	public void the_user_is_not_logged_in() {
-		assertNull(checkLogin());
+		assertFalse(checkLogin());
 	}
 		
 	@Given("the User had previously created two accounts")
@@ -44,15 +49,19 @@ public class CommonStepDef {
 	}
 	@Given("the User logs in successfully")
 	public void the_user_logs_in_successfully() {
-		assertNotNull(setUp.js.executeScript("return sessionStorage.getItem(\"username\");"));
+		if(checkLogin() == false) {
+			LogIn("testingExample","Password!1");
+		}
 	}
 	
 	public boolean checkLogin() {
-		return setUp.js.executeScript("return sessionStorage.getItem(\"username\")") != null;
+		return setUp.js.executeScript("return sessionStorage.getItem(\"username\");") != null;
 	}
 	
-	public void LogIn() {
+	public void LogIn(String Username, String Password) {
 		this.setUp.pageController.homePage.checkIfAtLoginPage();
-		//
+		this.setUp.pageController.logInPage.inputIntoUsername(Username);
+		this.setUp.pageController.logInPage.inputIntoPassword(Password);
+		this.setUp.pageController.logInPage.clickLogInButton();
 	}
 }
