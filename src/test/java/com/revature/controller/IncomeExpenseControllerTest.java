80d9affe6ebdaee8e8f0c60d981dc5908970dc03
George Roberts <ghr2710@gmail.com>
2022-05-26 17:18:51 -0500

    added tests from Seth's team

--- src/test/java/com/revature/controller/IncomeExpenseControllerTest.java
+++ src/test/java/com/revature/controller/IncomeExpenseControllerTest.java
@@ -1,13 +1,14 @@
 package com.revature.controller;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
+
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.revature.model.Transaction;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,11 +18,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
-import com.revature.dao.TransactionRepo;
+
 import com.revature.dto.TransactionDto;
-import com.revature.model.BankAccount;
-import com.revature.model.Transaction;
 import com.revature.service.TransactionService;
+
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class IncomeExpenseControllerTest {
@@ -35,12 +35,14 @@ class IncomeExpenseControllerTest {
 	static void init() {
 		lou = new IncomeExpenseController(txServ);
 	}
+
 	@Test
 	void negativeTransactionTest() {
 		TransactionDto dto = new TransactionDto(1, 1, -100.00, "SOS", Instant.now());
 		
 		assertThrows(ResponseStatusException.class, () -> lou.addTransaction(dto));
 	}
+
 	@Test
 	void badRequestTest() {
 		TransactionDto dto = new TransactionDto(0, 2, 1.00, "", Instant.now());
@@ -51,29 +53,28 @@ class IncomeExpenseControllerTest {
 		assertThrows(ResponseStatusException.class, () -> lou.addTransaction(dto2));
 		
 	}
-	
 	@Test
 	void addTransaction() {
 		TransactionDto transDto = Mockito.mock(TransactionDto.class);
 		TransactionService tranServ = Mockito.mock(TransactionService.class);
 		IncomeExpenseController inExpCont = new IncomeExpenseController(tranServ);
-		
+
 		when(transDto.getAccountId()).thenReturn(1);
 		when(transDto.getAmount()).thenReturn(1.0);
 		when(transDto.getDescription()).thenReturn("test");
-		
+
 		inExpCont.addTransaction(transDto);
 	}
-	
+
 	@Test
 	void getTransactions() {
 		TransactionService tranServ = Mockito.mock(TransactionService.class);
 		IncomeExpenseController inExpCont = new IncomeExpenseController(tranServ);
 		List<Transaction> transaction = new ArrayList();
 		int id = 1;
-		
+
 		List<Transaction> transactions = inExpCont.getTransactions(id);
 		assertEquals(transactions, transaction);
-		
+
 	}
 }
