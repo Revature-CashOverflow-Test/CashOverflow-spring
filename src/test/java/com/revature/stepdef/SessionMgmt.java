72d3e0624adba4a2866b6bd9a91d79bf655c02ea
Avery Grant <aniylaazaiahgrant@gmail.com>
2022-05-26 13:00:37 -0400

    established Session Management step def

--- src/test/java/com/revature/stepdef/SessionMgmt.java
+++ src/test/java/com/revature/stepdef/SessionMgmt.java
@@ -8,13 +8,11 @@ public class SessionMgmt {
 
 @When("the User clicks on different features")
 public void the_user_clicks_on_different_features() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    this.the_user_clicks_on_different_features();
 }
 
 @Then("the User stays logged in while exploring features without being logged out")
 public void the_user_stays_logged_in_while_exploring_features_without_being_logged_out() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    this.the_user_stays_logged_in_while_exploring_features_without_being_logged_out();
 }
 }
