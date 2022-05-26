eeb67373d4d1a8146cd2a2700e6b80c6fb63faaf
Avery Grant <aniylaazaiahgrant@gmail.com>
2022-05-26 15:35:12 -0400

    edited session managment test

--- src/test/java/com/revature/stepdef/SessionMgmt.java
+++ src/test/java/com/revature/stepdef/SessionMgmt.java
@@ -19,25 +19,20 @@ public class SessionMgmt {
 	
 @When("the User clicks on different features")
 public void the_user_clicks_on_different_features() {
-<<<<<<< HEAD
-    this.the_user_clicks_on_different_features();
-=======
+
 	this.setUp.pageController.homePage.clickRegisterOnNavBar();
     this.setUp.pageController.homePage.clickMyAccount();
     this.setUp.pageController.homePage.clickManageAccountBalanceNav();
     this.setUp.pageController.homePage.clickCreateBankAccountNav();
     this.setUp.pageController.homePage.clickAccountRegisterMenuBar();
     
-    this.setUp.pageController.homePage.clickLogOutButton();
->>>>>>> 5389c453500c91b0fd23ad83346ed287604151ae
+    
+
 }
 
 @Then("the User stays logged in while exploring features without being logged out")
 public void the_user_stays_logged_in_while_exploring_features_without_being_logged_out() {
-<<<<<<< HEAD
-    this.the_user_stays_logged_in_while_exploring_features_without_being_logged_out();
-=======
+
     assertTrue(this.setUp.pageController.homePage.clickAccountRegisterMenuBar());
->>>>>>> 5389c453500c91b0fd23ad83346ed287604151ae
-}
-}
+
+}}
